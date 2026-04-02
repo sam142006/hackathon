ac50d6573e91af73181ad9e2d301947f01ee6464
Shambhavi Pandey <shambhavip1411@gmail.com>
2026-04-02 20:31:53 +0530

    start session done

--- src/utils/interview.js
+++ src/utils/interview.js
@@ -1,10 +1,16 @@
 import { apiRequest } from './api';
 
-export const startInterviewSession = (token) =>
-  apiRequest('/api/interview/start-session', {
-    method: 'POST',
-    token,
-  });
+
 
   
+
+export const submitInterviewAnswer = (token, payload) =>
+  apiRequest('/api/interview/answer', {
+    method: 'POST',
+    token,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
 
