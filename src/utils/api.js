7308de130e6512505c6ceb60daa609cadb290b3a
Shambhavi Pandey <shambhavip1411@gmail.com>
2026-04-05 01:06:51 +0530

    project completed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://smarthire-hack.onrender.com';
+export const API_BASE_URL = 'https://smarthiree.duckdns.org';
 
 export const getAuthHeaders = (token, headers = {}) => ({
   ...headers,
@@ -8,8 +8,9 @@ export const getAuthHeaders = (token, headers = {}) => ({
 export const apiRequest = async (path, options = {}) => {
   const { token, headers = {}, ...restOptions } = options;
   const requestHeaders = token ? getAuthHeaders(token, headers) : headers;
+  const normalizedPath = path.startsWith('/') ? path : `/${path}`;
 
-  return fetch(`${API_BASE_URL}${path}`, {
+  return fetch(`${API_BASE_URL}${normalizedPath}`, {
     ...restOptions,
     headers: requestHeaders,
   });
